ac7ed7bbf8fc448bd489769321e8157f89ef8f1e
Nahida Zakir <nahidazakir15@gmail.com>
2024-01-09 17:20:20 +0600

    Function

---
+++ function.js
@@ -0,0 +1,24 @@
+// function sum(...items){
+//     if(items.length === 1 && Array.isArray(items[0])){
+//         items = [...items[0]];
+//     }
+//     return items.reduce((a,b)=> a+b);
+// }
+// console.log(sum([1,2,3,4]));
+
+
+// area of cicle
+
+// const circle = {
+//     radius:1,
+//     get area(){
+//         return Math.PI * this.radius * this.radius;
+//     }
+// }
+// console.log(circle.area);
+
+// Error Handling
+
+
+
+
