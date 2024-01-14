0c9139528b3f9ed7adc7f3731274c6a53eea4dcb
Nahida Zakir <nahidazakir15@gmail.com>
2024-01-14 22:50:45 +0600

    animation complete

--- Assignment_02/javascript.js
+++ Assignment_02/javascript.js
@@ -1,8 +1,20 @@
-function accept(){
-    document.getElementById('title').style.backgroundColor="black";
-    document.getElementById('title').style.color="white";
+async function accept(){
+    // document.body.classList.add('bghover');
+    const result1 = await progressBar();
+    setTimeout(removeProgress,3100);
 }
+
 function reject(){
     document.getElementById('title').style.backgroundColor="white";
     document.getElementById('title').style.color="red";
+}
+
+function progressBar(){
+    document.getElementById("progress").classList.add("animation");
+}
+function removeProgress(){
+    document.getElementById("progress").classList.remove("animation");
+    document.getElementById('title').style.backgroundColor="black";
+    document.getElementById('title').style.color="white";
+
 }
