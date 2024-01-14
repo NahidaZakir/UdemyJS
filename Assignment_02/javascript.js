3b1090260d0f72c07b5e097507315394e22fc40d
Nahida Zakir <nahidazakir15@gmail.com>
2024-01-14 20:54:46 +0600

    Button on click text css changed

---
+++ Assignment_02/javascript.js
@@ -0,0 +1,8 @@
+function accept(){
+    document.getElementById('title').style.backgroundColor="black";
+    document.getElementById('title').style.color="white";
+}
+function reject(){
+    document.getElementById('title').style.backgroundColor="white";
+    document.getElementById('title').style.color="red";
+}
