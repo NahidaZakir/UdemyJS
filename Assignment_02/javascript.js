ba9d1efc9ec3652d8318f703145b39005818cf6c
Nahida Zakir <nahidazakir15@gmail.com>
2024-01-14 22:54:32 +0600

    bg on click

--- Assignment_02/javascript.js
+++ Assignment_02/javascript.js
@@ -1,19 +1,22 @@
 async function accept(){
-    // document.body.classList.add('bghover');
     const result1 = await progressBar();
     setTimeout(removeProgress,3100);
 }
 
 function reject(){
-    document.getElementById('title').style.backgroundColor="white";
+    document.getElementById('title').style.backgroundColor="transparent";
     document.getElementById('title').style.color="red";
 }
 
 function progressBar(){
+    document.body.classList.add('bgclick');
+
     document.getElementById("progress").classList.add("animation");
 }
 function removeProgress(){
     document.getElementById("progress").classList.remove("animation");
+    document.body.classList.remove('bgclick');
+
     document.getElementById('title').style.backgroundColor="black";
     document.getElementById('title').style.color="white";
 
